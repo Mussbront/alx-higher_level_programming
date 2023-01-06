2708cb2e8014008bffab679eca5222278aca7ed7
Mussbront <vmogendi@gmail.com>
2023-01-06 15:59:30 -0800

    pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-       if len(sys.argv) == 1:
-            print(0)
-        elif len(sys.argv) == 2:
-            print(sys.argv[1])
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) == 2:
+        print(sys.argv[1])
     else:
         sum = 0
         for i in range(1, len(sys.argv)):
