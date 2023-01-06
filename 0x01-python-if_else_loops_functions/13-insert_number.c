5d8c4f794ab9baf88f8b2599e402be3a427603e1
Mussbront <vmogendi@gmail.com>
2023-01-06 14:36:34 -0800

    insert_number

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "lists.h"
+
+/**
+* insert_node - inserts a number into a sorted singly linked list
+* @ number: integer to be included in new node
+* Return:
+    */
+listint_t * insert_node(listint_t ** head, int number)
+{
+    listint_t * head
+
+    new -> next = current -> next
+    current -> next = new
+
+    return new
+}
