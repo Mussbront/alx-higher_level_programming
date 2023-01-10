75bef03d5028cc9bb56f9211c68e8bcfcc8a6e24
Mussbront <vmogendi@gmail.com>
2023-01-10 02:20:34 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,32 @@
 # include <stdlib.h>
 # include "lists.h"
 
-int is_palindrome(listint_t ** head)
+/**
+  * _palindrome - Checks if linked list is palindrome
+  * @head: head of list
+  * Return: Success (0)
+  */
+int _palindrome(listint_t **head)
 {
-    int size
-    int options[2048]
-    int i
+	int size;
+	int options[2048];
+	int i;
 
-    size = 0
-    if (head == NULL | | *head == NULL)
-    return (1)
+	size = 0;
+	if (head == NULL || *head == NULL)
+		return (1);
 
-    while (*head != NULL)
-    {
-        size++
-        options[size - 1] = (*head) -> n
-        head = &(*head) -> next
-    }
+	while (*head != NULL)
+	{
+		size++;
+		options[size - 1] = (*head)->n;
+		head = &(*head)->next;
+	}
 
-    for (i=0
-         i < size / 2
-         i++)
-    {
-        if (options[i] != options[size - i - 1])
-        return (0)
-    }
-
-    return (1)
+	for (i = 0; i < size / 2; i++)
+	{
+		if (options[i] != options[size - i - 1])
+			return (0);
+	}
+	return (1);
 }
